7d10503aa1102c5f09d3596e3f7bc90316771755
Victor May <mayvic@google.com>
2025-03-12 14:49:59 -0400

    Setting litellm stuff for vertex

--- java_migration/scripts/run_eval.py
+++ java_migration/scripts/run_eval.py
@@ -1,9 +1,16 @@
+import litellm
+from dotenv import load_dotenv
 import logging
+import os
 from java_migration.eval.eval_runner import EvalRunner
 from java_migration.utils import REPO_ROOT
 
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+    load_dotenv()
+
+    litellm.vertex_project = os.getenv("DEFAULT_VERTEXAI_PROJECT", None)
+    litellm.vertex_location = os.getenv("DEFAULT_VERTEXAI_LOCATION", None)
 
     dataset_path = REPO_ROOT / "data" / "migration_datasets" / "tiny_dataset.yaml"
     agent_cfg_path = REPO_ROOT / "java_migration" / "config" / "smol_default.yaml"
