fdfb8a1f9ea738fdec342adfd3d56e82c5b25964
Victor May <mayvic@gmail.com>
2025-06-30 18:16:10 +0000

    Filter permissive repos script

--- java_migration/scripts/filter_permissive.py
+++ java_migration/scripts/filter_permissive.py
@@ -1,12 +1,28 @@
 import csv
 from pathlib import Path
 
+import yaml
+
 full_dataset_path = Path("data/migration_datasets/full_dataset.yaml")
 
 license_file_path = Path("data/migration_datasets/permissive.csv")
 
 
+license_map = {}
 with open(license_file_path, "r") as csvfile:
-    reader = csv.DictReader(csvfile, fieldnames=None)
+    reader = csv.DictReader(csvfile, fieldnames=["id", "repo", "license"])
     for row in reader:
-        print(row)
+        license_map[row["repo"]] = row["license"]
+
+
+with open(full_dataset_path, "r") as f:
+    dataset = yaml.safe_load(f)
+
+out_dataset = []
+for item in dataset:
+    if item["repo_name"] in license_map:
+        item["license"] = license_map[item["repo_name"]]
+        out_dataset.append(item)
+
+with open("data/migration_datasets/full_dataset_with_license.yaml", "w") as f:
+    yaml.dump(out_dataset, f)
