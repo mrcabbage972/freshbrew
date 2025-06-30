9a474b8a65d3e13017b3ad7511ed0c8add460dd8
Victor May <mayvic@gmail.com>
2025-06-30 21:22:59 +0000

    Check checkouts script

--- java_migration/scripts/dataset_prep/check_checkouts.py
+++ java_migration/scripts/dataset_prep/check_checkouts.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 from java_migration.repo_workspace import RepoWorkspace
 from java_migration.utils import REPO_ROOT
 
-dataset_path = REPO_ROOT / "data/migration_datasets/full_dataset.yaml"
+dataset_path = REPO_ROOT / "data/migration_datasets/full_dataset_.yaml"
 
 failed_path = REPO_ROOT / "data" / "failed_checkout.jsonl"
 
@@ -22,8 +22,7 @@ for item in tqdm(dataset):
         with tempfile.TemporaryDirectory() as tmpdir:
             RepoWorkspace.from_git(item["repo_name"], workspace_dir=Path(tmpdir), commit_sha=item["commit"])
     except Exception as e:
-        errors.append({"repo_name": item["repo_name"], "error": e.args[0]._exception})
-
-with open(failed_path, "w") as fout:
-    for error in errors:
-        fout.write(json.dumps(error) + "\n")
+        error = {"repo_name": item["repo_name"], "error": str(e.args[0]._exception)}
+        print(f"Error in {item['repo_name']}: {e}")
+        with open(failed_path, "a") as fout:            
+            fout.write(json.dumps(error) + "\n")
---
+++ java_migration/scripts/dataset_prep/fix_commits.py
@@ -0,0 +1,37 @@
+import json
+import tempfile
+from pathlib import Path
+
+import yaml
+from tqdm import tqdm
+
+from java_migration.repo_workspace import RepoWorkspace
+from java_migration.utils import REPO_ROOT
+from java_migration.eval.utils import  recover_safe_repo_name
+
+dataset_path = REPO_ROOT / "data/migration_datasets/full_dataset.yaml"
+
+failed_path = REPO_ROOT / "data/missing_commits.txt"
+result_path = REPO_ROOT / "data/migration_datasets/full_dataset_.yaml"
+
+with open(dataset_path, "r") as fin:
+    dataset = yaml.safe_load(fin.read())
+
+with open(failed_path) as fin:
+    failed = [x.split(",") for x in fin.readlines()]
+    failed_dict = {}
+    for x in failed:
+        try:
+            failed_dict[recover_safe_repo_name(x[0])] = x[1].strip()
+        except Exception as e:
+            print(f"Error in {x[0]}: {e}")
+
+
+for item in tqdm(dataset):
+    if item["repo_name"] in failed_dict:
+        item["commit"] = failed_dict[item["repo_name"]]
+        print(f"Using commit {item['commit']} for {item['repo_name']}")
+
+with open(result_path, "w") as fout:
+    fout.write(yaml.dump(dataset))
+
