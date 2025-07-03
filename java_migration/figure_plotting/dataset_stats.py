3711fdcd491ed7c084bdab19327d6dd10d1ff2bd
Victor May <mayvic@gmail.com>
2025-07-03 18:51:49 +0000

    wip

--- java_migration/figure_plotting/dataset_stats.py
+++ java_migration/figure_plotting/dataset_stats.py
@@ -22,12 +22,28 @@ cov_data_path = REPO_ROOT / "data/migration_datasets/cov_data.csv"
 with open(dataset_path) as fin:
     ds = yaml.safe_load(fin)
 
-all_repo_features = [x["repo_features"] | {"repo": x["repo_name"]} | {"license": x["license"]} for x in ds]
+all_repo_features = [
+    x["repo_features"] | {"repo": x["repo_name"]} | {"license": x["license"]} | {"commit_date": x["commit_date"]}
+    for x in ds
+]
 df = pd.DataFrame(all_repo_features)
 
 df_cov = pd.read_csv(cov_data_path)
 df_cov["repo"] = df_cov.repo.apply(recover_safe_repo_name)
 df = df.merge(df_cov, on="repo")
+
+date_years = df["commit_date"].apply(lambda x: x.split("-")[0])
+date_year_counts = date_years.value_counts(sort=False)
+date_year_counts = date_year_counts.sort_index(axis=0)
+
+plot_counts(
+    bins=date_year_counts.index.tolist(),
+    counts=date_year_counts.tolist(),
+    xlabel="Commit Date",
+    ylabel="Repositories",
+    output_path=REPO_ROOT / "java_migration/figures" / "commit_date_hist.pdf",
+)
+
 # plot_deps_per_repo(df, REPO_ROOT / "java_migration/figures/deps_per_repo.pdf")
 # plot_num_files(df, REPO_ROOT / "java_migration/figures/files_per_repo.pdf")
 
---
+++ setup_env_cloud_ws.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install maven
+sudo update-java-alternatives --set  /usr/lib/jvm/temurin-17-jdk-amd64
