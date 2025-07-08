11c61d7614f797ed4f3b8a2eb8e1a48e0fb41dd4
Victor May <mayvic@gmail.com>
2025-07-08 18:36:01 +0000

    wip

--- java_migration/figure_plotting/migration_success_table.py
+++ java_migration/figure_plotting/migration_success_table.py
@@ -17,11 +17,14 @@ for experiment_path in experiment_paths:
     compile_success_rate = 1.0 * metrics["compile"]["succeeded"] / metrics["compile"]["started"]
     test_success_rate = 1.0 * metrics["test"]["succeeded"] / metrics["compile"]["started"]
 
+    num_success = len([x["status"] for x in cov["repo_results"].values() if x["status"] == "JobStatus.SUCCESS" and x["coverage"]["cov_guard_pass"] is True])
+    cov_guard_pass_rate = 1.0 * num_success / (metrics["compile"]["started"] - cov["job_fails"])
+
     exp_results.append(
         {
             "java_version": exp_info["target_jdk_version"],
             "model": exp_info["model"],
-            "cov_guard_pass_rate": cov["cov_guard_pass_rate"],
+            "cov_guard_pass_rate": cov_guard_pass_rate,
             "compile_success_rate": compile_success_rate,
             "test_success_rate": test_success_rate,
         }
@@ -35,27 +38,32 @@ pivoted_df = df.pivot(
 
 
 pivoted_df = pivoted_df[
-    [
-        ("cov_guard_pass_rate", 17),
+    [       
         ("compile_success_rate", 17),
         ("test_success_rate", 17),
-        ("cov_guard_pass_rate", 21),
+         ("cov_guard_pass_rate", 17),        
         ("compile_success_rate", 21),
         ("test_success_rate", 21),
+        ("cov_guard_pass_rate", 21),
     ]
 ]
 
 
 # --- LaTeX Generation ---
 latex_string = r"""
-\begin{table}[htbp]
+\begin{table*}[htbp]
 \centering
 \caption{Performance on Migration}
-\begin{tabular}{l ccc ccc}
+\begin{tabular}{
+    l    
+    c c     
+    > {\columncolor{HighlightGray}}c    
+    c c 
+    > {\columncolor{HighlightGray}}c}
 \toprule
 \textbf{Model} & \multicolumn{3}{c}{\textbf{JDK 17 Success Rate}} & \multicolumn{3}{c}{\textbf{JDK 21 Success Rate}} \\
 \cmidrule(lr){2-4} \cmidrule(lr){5-7}
-& \textbf{Cov Guard} & \textbf{Compilation} & \textbf{Tests} & \textbf{Cov Guard} & \textbf{Compilation} & \textbf{Tests}\\
+& \textbf{Compilation} & \textbf{Tests} & \textbf{Cov Guard} & \textbf{Compilation} & \textbf{Tests} & \textbf{Cov Guard}\\
 \midrule
 """
 
@@ -74,6 +82,6 @@ for index, row in pivoted_df.iterrows():
 latex_string += r"""\bottomrule
 \end{tabular}
 \label{tab:model_performance}
-\end{table}"""
+\end{table*}"""
 
 print(latex_string)
