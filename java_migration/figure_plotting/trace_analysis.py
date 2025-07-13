cbf07abdec296196e7aae18489d82ff4e6057651
Victor May <mayvic@gmail.com>
2025-07-13 01:09:07 +0000

    wip

--- java_migration/figure_plotting/trace_analysis.py
+++ java_migration/figure_plotting/trace_analysis.py
@@ -1,14 +1,17 @@
 import yaml
 
 from java_migration.eval.smol_log_parser import parse_log
-from java_migration.figure_plotting.figure_utils import plot_boxplot_grid, plot_histogram_grid
+from java_migration.figure_plotting.figure_utils import plot_boxplot_grid
 from java_migration.utils import REPO_ROOT
 
-exp_result_paths = ["data/experiments/2025-07-07/18-20-39-stoic-feistel-2",
-                    "data/experiments/2025-07-09/smol-openai-gpt-4.1-target-jdk-17",
-                     "data/experiments/2025-07-09/smol-openai-o3-mini-target-jdk-17"]
+exp_result_paths = [
+    "data/experiments/2025-07-07/18-20-39-stoic-feistel-2",
+    "data/experiments/2025-07-09/smol-openai-gpt-4.1-target-jdk-17",
+    "data/experiments/2025-07-09/smol-openai-o3-mini-target-jdk-17",
+]
 
 models = ["Gemini 2.0 Flash", "GPT 4.1", "O3-mini"]
+
 
 def get_logs(exp_result_path):
     logs = []
@@ -21,6 +24,7 @@ def get_logs(exp_result_path):
         log_path = entry / "stdout.log"
         logs.append(parse_log(log_path.read_text()))
     return logs
+
 
 all_logs = []
 for p in exp_result_paths:
@@ -38,7 +42,7 @@ for p in exp_result_paths:
 #     figs_y=1,
 # )
 
-num_toks = [[log.steps[-1].meta.input_tokens // 1000 for log in logs]  for logs in all_logs]
+num_toks = [[log.steps[-1].meta.input_tokens // 1000 for log in logs] for logs in all_logs]
 num_steps = [[len(log.steps) for log in logs] for logs in all_logs]
 
 
@@ -55,9 +59,8 @@ num_steps = [[len(log.steps) for log in logs] for logs in all_logs]
 # )
 
 
-
 plot_boxplot_grid(
-    data_list=num_steps,
+    data_list=num_steps,  # type: ignore
     subplot_titles=models,
     figure_ylabel="Steps",
     output_path=REPO_ROOT / "java_migration/figures" / "step_boxplots.pdf",
